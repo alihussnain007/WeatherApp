6856d948738ec90ba91531aad7af0fd63aa10ecf
alihussnain007 <ali.hussnain@arrivy.com>
2025-01-02 14:42:52 +0500

    Normal Commit

---
+++ WeatherApp/model/HourlyWeatherRealmModel.swift
@@ -0,0 +1,8 @@
+//
+//  HourlyWeatherRealmModel.swift
+//  WeatherApp
+//
+//  Created by Arrivy on 01/01/2025.
+//
+
+import Foundation
---
+++ WeatherApp/viewModel/HourlyWeatherCellCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  HourlyWeatherCellCollectionViewCell.swift
+//  WeatherApp
+//
+//  Created by Arrivy on 01/01/2025.
+//
+
+import UIKit
+
+class HourlyWeatherCellCollectionViewCell: UICollectionViewCell {
+    
+}
